8171b00dcbfe9aee0e0efd91e639d87f900d1b44
arash <arash.ask.94@gmail.com>
2021-07-28 18:53:31 +0430

    first commit

---
+++ simple_form_Qt.py
@@ -0,0 +1,31 @@
+import sys
+ 
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialogButtonBox
+from PyQt5.QtWidgets import QFormLayout
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QVBoxLayout
+
+class Dialog(QDialog):
+    def __init__(self , parent = None):
+        
+        super().__init__(parent)
+        self.setWindowTitle('QDialog')
+        dlgLayout = QVBoxLayout()
+        formLayout = QFormLayout()
+        formLayout.addRow('name :' , QLineEdit())
+        formLayout.addRow('age :' , QLineEdit())
+        formLayout.addRow('job:' , QLineEdit())
+        dlgLayout.addLayout(formLayout)
+        btns = QDialogButtonBox()
+        btns.setStandardButtons( QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
+        dlgLayout.addWidget(btns)
+        self.setLayout(dlgLayout)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dlg = Dialog()
+    dlg.show()
+    sys.exit(app.exec_())
